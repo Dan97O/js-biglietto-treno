48150ecb0315cf42ec043b697fb3e45e24b1591d
Dan97O <dan97-oddo@live.it>
2023-03-08 15:23:15 +0100

    price ticket young and price ticket elders

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,35 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 // sconto 20% ai minorenni
 // sconto 40% over 65
 // output con massimo due decimali 
+
+
+// numero km da percorrere
+const kilometersToGo = prompt('Quanti chilometri vuoi percorrere?');
+console.log(kilometersToGo);
+
+// età del passeggiero
+const userAge = prompt('Quanti anni hai?');
+console.log(userAge);
+
+// calcolare il prezzo del viaggio 1km = 0.21€
+
+const priceTicket = kilometersToGo * 0.21
+console.log(priceTicket);
+
+
+// -20% <18 anni
+// -40% >65 anni
+if(userAge < 18){
+    const priceTicketYoung = (priceTicket / 100 * 20);
+    console.log(priceTicketYoung);
+    alert(`il tuo prezzo giovane é ${priceTicketYoung} `)
+} else if(userAge >65){
+    const priceTicketOld = (priceTicket / 100 * 40);
+    console.log(priceTicketOld);
+    alert(`il tuo prezzo anziani é ${priceTicketOld} `)
+} else{
+    alert(`il tuo prezzo normale é ${priceTicket} `)
+}
+
+
+
